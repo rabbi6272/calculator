c07171cdcf371d743c082d8959e152aea4a19f90
Rabbi <140103650+rabbi6272@users.noreply.github.com>
2023-09-03 01:32:12 +0600

    Add files via upload

---
+++ source.js
@@ -0,0 +1,5 @@
+function loveCalc () {
+    document.querySelectorAll(".text1").innerHTML = "type";
+    var number1 = Math.floor(Math.random() * 100);
+    return  document.getElementById("txt").innerHTML = number1 + "%";
+}
