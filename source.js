075d304ffe9976675eeddaac6eb294ceeff9d38d
Rabbi <140103650+rabbi6272@users.noreply.github.com>
2023-09-03 01:35:28 +0600

    Delete source.js

--- source.js
+++
@@ -1,5 +0,0 @@
-function loveCalc () {
-    document.querySelectorAll(".text1").innerHTML = "type";
-    var number1 = Math.floor(Math.random() * 100);
-    return  document.getElementById("txt").innerHTML = number1 + "%";
-}
